60aa274c5e0a3c81c0288fa0a838283d482669dc
marilia <marilia.vieira10@aluno.ifce.edu.br>
2026-03-12 10:44:53 -0300

    trabalho

---
+++ backend/src/app.js
@@ -0,0 +1,18 @@
+import express from "express"; 
+const app = express ();
+
+app.get("/", (req, res) => {
+res.send({message:"Servidor em Execução"*, success: true});
+
+});
+app.get("/equipe", (req,res) => {
+    res.send([
+{nome:"Yasmin", curso:"TI"},
+{nome:"Emanuelly", curso:"TI"},
+{nome:"Marilia", curso:"TI"},
+    ]);
+});
+
+app.listen(3000, () => {
+    console.log("Server running on localhost:3000");
+});
