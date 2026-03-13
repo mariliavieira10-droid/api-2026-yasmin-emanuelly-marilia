d9a037eda2b0c494114d53dd7bf9ccffc1a46e60
emanbuelly <emanuelly.costa07@aluno.ifce.edu.br>
2026-03-13 09:59:58 -0300

    teste

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,6 +13,6 @@ app.get("/equipe", (req,res) => {
     ]);
 });
 
-app.listen(3000, () => {
-    console.log("Server running on localhost:3000");
+app.listen(process.env.PORT || 3000, () => {
+console.log("Server running on localhost:3000");
 });
