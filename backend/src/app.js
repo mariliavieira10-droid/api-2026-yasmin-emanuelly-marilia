a2a620b87c7f54f5b6ad182da713f3749b4967e0
Emanuelly <emanuelly.costa07@aluno.ifce.edu.br>
2026-03-17 11:25:10 -0300

    rotas

--- backend/src/app.js
+++ backend/src/app.js
@@ -18,10 +18,19 @@ console.log("Server running on localhost:3000");
 });
 
 
-app.get('/yasmin-emanuelly/marilia', (req, res) => {
+app.get('/yasmin-emanuelly-marilia/marilia', (req, res) => {
   res.json({
     nome: "Marília",
     matricula: "20251131000302",
     info: "Amo o Corinthians"
+  });
+});
+
+
+app.get('/yasmin-emanuelly-marilia/emanuelly', (req, res) => {
+  res.json({
+    nome: "Emanuelly",
+    matricula: "20251131000256",
+    info: "Amo o Ceará Sport Club"
   });
 });
