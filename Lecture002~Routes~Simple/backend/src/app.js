2d523f7813c31b6077ef6d947d27543794133053
OViniSA <oviniciussilvaalves@gmail.com>
2026-03-26 11:04:32 -0300

    nova aula

---
+++ Lecture002~Routes~Simple/backend/src/app.js
@@ -0,0 +1,16 @@
+import express from "express";
+const app = express();
+app.get("/equipe", (req, res) => {
+res.send([
+{nome: "Fulano", curso: "TI"},
+{nome: "Fulano", curso: "TI"},
+{nome: "Ciclano", curso: "TI"},
+]);
+app.get("/", (req, res) => {
+res.send({message: "Servidor em Execução"*, success: true });
+});
+});
+app.listen(3000, () => {
+console.log("Server running on localhost:3000");
+});
+
